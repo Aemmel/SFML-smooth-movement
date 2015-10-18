a5a722810fb412091016456ab417b475b618054b
Aemmel <emilkasper@web.de>
2015-10-18 15:10:20 +0200

    Create Global_Constants.hpp

---
+++ Global_Constants.hpp
@@ -0,0 +1,17 @@
+#ifndef GLOBAL_CONSTANTS_HPP
+#define GLOBAL_CONSTANTS_HPP
+
+/*
+ * save all constants here which should be accesible from everywhere, only things that are really necessary
+ */
+
+namespace C
+{
+
+//window globals
+const float Window_Width = 900;
+const float Window_Height = 900;
+
+} //end namespace C
+
+#endif //GLOBAL_CONSTANTS_HPP
